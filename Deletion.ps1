c28571cee629af7f3d78013207f75d43039de8c8
rahulkanoj <98455578+rahulkanoj@users.noreply.github.com>
2024-12-05 14:28:21 +0530

    Add files via upload

---
+++ Deletion.ps1
@@ -0,0 +1,30 @@
+﻿$Subscription="CZ MED27 - MUC - Curie - PreProd - EU"
+$MyResourceGroup="czm27-cur-pp-euw"
+$l="West Europe"
+az account set --subscription $Subscription
+az account show --output table
+
+Write-Host "Deletion of Function apps in progress `n"
+$funclist=(az functionapp list --resource-group "$MyResourceGroup" --query "[].{FunctionName:repositorySiteName}" -o tsv)
+foreach ($functions in $funclist) {
+$functions
+#az functionapp delete --name $functions --resource-group "$MyResourceGroup"
+}
+Write-Host "Function apps have been deleted `n"
+
+Write-Host "Deletion of App Service Plan is started `n"
+$planlist=(az appservice plan list --resource-group $MyResourceGroup --query "[].{Name:name}" -o tsv)
+foreach ($plans in $planlist) {
+$plans
+#az appservice plan delete --name $plans --resource-group $MyResourceGroup
+}
+Write-Host "Deletion of App Service Plan is completed `n"
+
+Write-Host "Deletion of API Management service is started `n"
+$APIMlist=(az apim list --resource-group $MyResourceGroup --query "[].{Name:name}" -o tsv)
+$APIMlist.l
+foreach ($APIM in $APIMlist) {
+#az apim delete -n $APIM -g $MyResourceGroup
+#az apim deletedservice purge --location $l --service-name $APIM
+}
+Write-Host "Deletion of API Management service has been completed `n"
